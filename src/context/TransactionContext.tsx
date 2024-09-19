ee2fdaa9075ef62e5eab5a92f3903b8d9d24c94f
ernluiz <lf.ern@hotmail.com>
2024-09-19 01:12:47 -0300

    corrigido chart

--- src/context/TransactionContext.tsx
+++ src/context/TransactionContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
-import ExpenseIncomeChart from '../components/chart/ExpenseIncome';
+import ExpenseIncomeChart from '../components/Chart/ExpenseIncome';
 
 export interface Transaction {
   id: number;
